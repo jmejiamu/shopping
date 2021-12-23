6255cc1fdb66a812f5ac126c3dd080437497bd49
jmejiamu <jmejiamu@gmail.com>
2021-12-22 22:31:12 -0800

    added components folder

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Shopping Store
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./pages/cart-products/index.html">
+                <i className="bi bi-cart"></i>
+                <span
+                  id="addItem"
+                  className="badge badge-pill bg-danger"
+                ></span>
+              </a>
+            </li>
+
+            <li className="nav-item mx-2">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </li>
+            <li className="nav-item">
+              <button
+                className="btn btn-outline-primary my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
