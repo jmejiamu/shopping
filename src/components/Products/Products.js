6154b337c1999d36d674a020e22a5df2bc35e34d
jmejiamu <jmejiamu@gmail.com>
2021-12-24 23:28:54 -0800

    added products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,137 @@
 import React from "react";
-
+import product from "../../assets/products.jpg";
 const Products = () => {
-  return <div></div>;
+  return (
+    <main>
+      <section class="container">
+        <h1 id="checkitout" class="text-center mt-5 mb-5">
+          Products
+        </h1>
+        <div class="row">
+          <div class="col-md-4 mb-5">
+            <div class="thumbnail card card-style">
+              <a href="./pages/product-details/index.html">
+                <div class="bg-image hover-zoom">
+                  <img src={product} alt="Lights" style={{ width: "100%" }} />
+                </div>
+              </a>
+              <div class="caption d-flex justify-content-between">
+                <p>Lorem ipsum..T.</p>
+                <p>$10</p>
+              </div>
+              <button
+                type="button"
+                onclick="updateCartItem();"
+                class="btn btn-primary btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="thumbnail card card-style">
+              <a href="./pages/product-details/index.html">
+                <div class="bg-image hover-zoom">
+                  <img src={product} alt="Lights" style={{ width: "100%" }} />
+                </div>
+              </a>
+              <div class="caption d-flex justify-content-between">
+                <p>Lorem ipsum...</p>
+                <p>$10</p>
+              </div>
+              <button
+                type="button"
+                onclick="updateCartItem();"
+                class="btn btn-primary btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="thumbnail card card-style">
+              <a href="./pages/product-details/index.html">
+                <div class="bg-image hover-zoom">
+                  <img src={product} alt="Lights" style={{ width: "100%" }} />
+                </div>
+              </a>
+              <div class="caption d-flex justify-content-between">
+                <p>Lorem ipsum...</p>
+                <p>$10</p>
+              </div>
+              <button
+                type="button"
+                onclick="updateCartItem();"
+                class="btn btn-primary btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="thumbnail card card-style">
+              <a href="./pages/product-details/index.html">
+                <div class="bg-image hover-zoom">
+                  <img src={product} alt="Lights" style={{ width: "100%" }} />
+                </div>
+              </a>
+              <div class="caption d-flex justify-content-between">
+                <p>Lorem ipsum...</p>
+                <p>$10</p>
+              </div>
+              <button
+                type="button"
+                onclick="updateCartItem();"
+                class="btn btn-primary btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="thumbnail card card-style">
+              <a href="./pages/product-details/index.html">
+                <div class="bg-image hover-zoom">
+                  <img src={product} alt="Lights" style={{ width: "100%" }} />
+                </div>
+              </a>
+              <div class="caption d-flex justify-content-between">
+                <p>Lorem ipsum...</p>
+                <p>$10</p>
+              </div>
+              <button
+                type="button"
+                onclick="updateCartItem();"
+                class="btn btn-primary btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="thumbnail card card-style">
+              <a href="./pages/product-details/index.html">
+                <div class="bg-image hover-zoom">
+                  <img src={product} alt="Lights" style={{ width: "100%" }} />
+                </div>
+              </a>
+              <div class="caption d-flex justify-content-between">
+                <p>Lorem ipsum...</p>
+                <p>$10</p>
+              </div>
+              <button
+                type="button"
+                onclick="updateCartItem();"
+                class="btn btn-primary btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Products;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "../components/Navbar/Navbar";
+import Products from "../components/Products/Products";
 const Home = () => {
   return (
     <>
       <NavBar />
+      <Products />
     </>
   );
 };
