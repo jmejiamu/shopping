2fef1e534b1c811533922c7b00516449dea970e9
jmejiamu <jmejiamu@gmail.com>
2022-01-03 15:10:03 -0800

    remove commented code

--- src/components/ProDetails/ProDetails.js
+++ src/components/ProDetails/ProDetails.js
@@ -27,6 +27,7 @@ const ProDetails = () => {
   useEffect(() => {
     getOneProduct();
   }, []);
+  //Use date to create a unique id
   const uniqueId = new Date();
   return (
     <main class="container mt-5">
@@ -124,84 +125,6 @@ const ProDetails = () => {
           </div>
         </div>
       </section>
-
-      {/* <section>
-        <h1 class="text-center">Most Viewed</h1>
-        <div class="row">
-          <div class="col-md-4 mb-5">
-            <div class="thumbnail card card-style">
-              <a href="../../asset/products.jpg">
-                <div class="bg-image hover-zoom">
-                  <img
-                    src="../../asset/products.jpg"
-                    alt="Lights"
-                    style={{ width: "100%" }}
-                  />
-                </div>
-              </a>
-              <div class="caption d-flex justify-content-between">
-                <p>Lorem ipsum..T.</p>
-                <p>$10</p>
-              </div>
-              <button
-                type="button"
-                onclick="updateCartItem();"
-                class="btn btn-primary btn-block"
-              >
-                Add
-              </button>
-            </div>
-          </div>
-          <div class="col-md-4 mb-5">
-            <div class="thumbnail card card-style">
-              <a href="../../asset/products.jpg">
-                <div class="bg-image hover-zoom">
-                  <img
-                    src="../../asset/products.jpg"
-                    alt="Lights"
-                    style={{ width: "100%" }}
-                  />
-                </div>
-              </a>
-              <div class="caption d-flex justify-content-between">
-                <p>Lorem ipsum...</p>
-                <p>$10</p>
-              </div>
-              <button
-                type="button"
-                onclick="updateCartItem();"
-                class="btn btn-primary btn-block"
-              >
-                Add
-              </button>
-            </div>
-          </div>
-          <div class="col-md-4 mb-5">
-            <div class="thumbnail card card-style">
-              <a href="../../asset/products.jpg">
-                <div class="bg-image hover-zoom">
-                  <img
-                    src="../../asset/products.jpg"
-                    alt="Lights"
-                    style={{ width: "100%" }}
-                  />
-                </div>
-              </a>
-              <div class="caption d-flex justify-content-between">
-                <p>Lorem ipsum...</p>
-                <p>$10</p>
-              </div>
-              <button
-                type="button"
-                onclick="updateCartItem();"
-                class="btn btn-primary btn-block"
-              >
-                Add
-              </button>
-            </div>
-          </div>
-        </div>
-      </section> */}
     </main>
   );
 };
